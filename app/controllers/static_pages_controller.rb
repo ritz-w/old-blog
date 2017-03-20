755d214106fc60e9c9bbfc692f562fbddc0a010e
Ritz-W <ritz.wu@network.rca.ac.uk>
2017-03-20 13:03:40 +0900

    added about page, improved front page, added dates

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,8 @@ class StaticPagesController < ApplicationController
 def index
   @posts = Post.all
   end
+
+  def about
+@posts = Post.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   get 'static_pages/index'
+  get 'static_pages/about'
 
 root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
